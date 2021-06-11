3c870338bd0b3c371f84a2d79880c47fba6470a7
Kristy Lee <leekristy01@gmail.com>
2021-06-11 16:33:27 -0400

    First attempt to infinite scrolling

--- src/App.js
+++ src/App.js
@@ -1,29 +1,92 @@
-import logo from './logo.svg';
 import './App.css';
 // import PinterestPins from './data'
-import React, { useState } from 'react'
+
+// We need an element reference to the last pin element in order to know which element is the last one
+// When reference changes to the last pin when we reach to the bottom scroll, component is not rerendered
+import React, { useState, useRef, useCallback } from 'react' // useRef is to store a value that persists after each render
 import useIntersect from './useIntersect'
 
 function App() {
   const [query, setQuery] = useState('')
   const [startSliceNumber, setStartSliceNumber] = useState(0)
-  const [endSliceNumber, setEndSliceNumber] = useState(100)
+  const [endSliceNumber, setEndSliceNumber] = useState(10)
 
-  useIntersect(query, startSliceNumber, endSliceNumber)
+  const {loading, error, pins, hasMore } = useIntersect(query, startSliceNumber, endSliceNumber)
+
+  const observer = useRef(null) // null initially - then set a reference to the last book element
+
+  const lastPinElementRef = useCallback(node => {
+    console.log(19, loading)
+    if(loading) return // If we are loading we do not want to trigger infinite scrolling
+
+    const options = {
+      root: null,
+      // rootMargin: "20px",
+      threshold: 0
+    }
+
+    // Disconnect the reference from the previous last pin 
+    if (observer.current) observer.current.disconnect()
+
+    observer.current = new IntersectionObserver(entries => {
+      // Only observing one target element 
+      console.log(33, hasMore)
+      console.log(34, entries[0].isIntersecting)
+      if (entries[0].isIntersecting && hasMore) {
+        console.log('Visible')
+        setStartSliceNumber(prevStartSliceNumber => prevStartSliceNumber + 10)
+        console.log(38, startSliceNumber)
+        setEndSliceNumber(prevEndSliceNumber => prevEndSliceNumber + 10)
+        console.log(40, endSliceNumber)
+      }
+    }, options)
+
+    // If we have a last element, we want to reference it and observe that element 
+    if (node) observer.current.observe(node)
+
+  }, [loading, hasMore])
+
+
 
   const handleQuery = (event) => {
-    setQuery(event.target.value)
+    setQuery(event.target.value) // Each letter a user types in the input box will update the query, which will call useIntersect 
 
     // Reset the page back to the beginning
     setStartSliceNumber(0) 
-    setEndSliceNumber(100)
+    setEndSliceNumber(10)
   }
+// 19 false, 64 true, 33 true, 34 false  
+// 33 true 34 true Visible 102 undefined undefined 112 true 19 false 19 false 33 true 34 false
+// 33 true 34 true Visible 102 undefined undefined 112 true
 
   return (
     <div>
-    <h1>Pinterest</h1>
-    <input type="text" onChange={handleQuery}></input>
-    {/* <PinterestPins/> */}
+      <h1>Pinterest</h1>
+      <input type="text" value={query} onChange={handleQuery}></input>
+      {/* <PinterestPins/> */}
+      
+      <div>{
+          pins.map((pin, index) => {
+            if (pins.length === index + 1) { // This means we are at our last pin
+              return (
+                <>
+                <div style={{width: '200px', height: '100px', backgroundColor:'yellow'}} ref={lastPinElementRef} key={pin.id}>{pin.title}</div>
+                <p>**************************</p>
+                </>
+              )
+            } else {
+              return (
+                <>
+                <div style={{width: '200px', height: '100px', backgroundColor:'yellow'}} key={pin.id}>{pin.title}</div>
+                <p>--------------------------</p>
+                </>
+              )
+            }
+          })
+      }</div>
+
+      <div>{loading && 'Loading...'}</div>
+      <div>{error && 'Error'}</div>
 
     </div>
   );
--- src/useIntersect.jsx
+++ src/useIntersect.jsx
@@ -1,39 +1,124 @@
 import { useEffect, useState } from 'react'
 
 export default function useIntersect(query, startSliceNumber, endSliceNumber) { // When your scrolling hits the bottom, fetch the next page of results
+    const [loading, setLoading] = useState(true)
+    const [error, setError] = useState(false)
+    const [pins, setPins] = useState([])
+    const [allPins, setAllPins] = useState([])
+    const [allQueryPins, setAllQueryPins] = useState([])
+    const [hasMore, setHasMore] = useState(false)
+
     useEffect(() => {
+        setPins([])
+    }, [query])
+
+    useEffect(() => {
+        // Every time we are making a brand new request we are loading so set loading to true, 
+        // and we do not have an error for a new request do set error to false
+        setLoading(true)
+        setError(false)
+
+        const abortController = new AbortController()
+        const signal = abortController.signal
+
         const getPins = async () => {
-            const pinsResponse = await fetch('nyc_ttp_pins.json', {
-                method: 'GET',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Accept': 'application/json'
 
-                }
-            })
+            try {
+                const pinsResponse = await fetch('nyc_ttp_pins.json', {
+                    method: 'GET',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Accept': 'application/json'
 
-            const pinsJSON = await pinsResponse.json()
-            console.log(pinsJSON)
-
-            if (query) {
-                const getQueryResults = pinsJSON.filter((pin) => {
-                    filteredPin = pin['pin_join']['visual_annotation'].filter(word => word.includes(query))
-                    if (filteredPin.length > 0) {
-                        return filteredPin
-                    }
+                    },
+                    signal: signal
                 })
-                setPins(getQueryResults.slice(startSliceNumber, endSliceNumber))
-            } else {
-                // Get only 100 or less results every time we scroll to the bottom
-                setPins(pinsJSON.slice(startSliceNumber, endSliceNumber))
-            }
-            
 
+                const pinsJSON = await pinsResponse.json()
+                console.log(pinsJSON)
+
+                if (query) {
+                    const getQueryResults = pinsJSON.filter((pin) => {
+                        const filteredPin = pin['pin_join']['visual_annotation'].filter(word => word.includes(query))
+                        if (filteredPin.length > 0) {
+                            return filteredPin
+                        } 
+                    })
+                    console.log(47, getQueryResults)
+
+                    setAllQueryPins(getQueryResults) // Hold all the results
+
+                    setPins(getQueryResults.slice(startSliceNumber, endSliceNumber))
+                    // setPins(prevPins => {
+                    //     return [...new Set([...prevPins, ...getQueryResults.map(pin => pin)])]
+                    // })
+                    setHasMore(!(startSliceNumber >= getQueryResults.length))
+                    setLoading(false)
+                } else {
+                    setAllPins(pinsJSON)
+                    // Get only 100 or less results every time we scroll to the bottom
+                    setPins(pinsJSON.slice(startSliceNumber, endSliceNumber))
+                    // const somePins = pinsJSON.slice(startSliceNumber, endSliceNumber)
+                    // setPins(prevPins => {
+                    //     return [...new Set([...prevPins, ...somePins.map(pin => pin)])]
+                    // })
+                    console.log(64, !(startSliceNumber >= pinsResponse.length))
+                    setHasMore(!(startSliceNumber >= pinsResponse.length))
+                    setLoading(false)
+                }
+            } catch (error) {
+                if (error.name === 'AbortError') {
+                    console.log('Request was cancelled')
+                    return
+                }
+                setError(true)
+            }
            
         }
-
+        
         getPins()
-    }, [query, pageNumber]) // Want to fetch results everytime we do a search or scrolling
 
-    return null
+        return () => abortController.abort()
+
+    }, [query])
+    
+    useEffect(() => {
+        // Every time we are making a brand new request we are loading so set loading to true, 
+        // and we do not have an error for a new request do set error to false
+        setLoading(false)
+        setError(false)
+
+        const getNextPins =  (query, startSliceNumber, endSliceNumber) => {
+
+            if (query) {
+                console.log(94, query)
+                const nextQueryPins = allQueryPins.slice(startSliceNumber, endSliceNumber)
+
+                setPins(prevPins => {
+                    return [...new Set([...prevPins, ...nextQueryPins.map(pin => pin)])]
+                })
+                setHasMore(!(startSliceNumber >= allQueryPins.length))
+                // setLoading(false)
+            } else {
+                console.log(102, startSliceNumber, endSliceNumber)
+                // Get only 10 or less results every time we scroll to the bottom
+                const nextPins = allPins.slice(startSliceNumber, endSliceNumber)
+
+                setPins(prevPins => {
+                    return [...new Set([...prevPins, ...nextPins.map(pin => pin)])]
+                })
+
+                console.log(112, !(startSliceNumber >= allPins.length))
+                
+                setHasMore(!(startSliceNumber >= allPins.length))
+                // setLoading(false)
+            }
+        }
+        
+        getNextPins()
+
+    }, [query, startSliceNumber]) // Want to fetch results everytime we do a search or scrolling
+
+    return { loading, error, pins, hasMore }
 }
+
