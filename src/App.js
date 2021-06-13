5aec34f86665c8cec57154723e9472545ab1354a
Kristy Lee <leekristy01@gmail.com>
2021-06-13 19:08:59 -0400

    URL and slicing numbers provided by user

--- src/App.js
+++ src/App.js
@@ -6,113 +6,12 @@ import './App.css';
 import React, { useState, useRef, useEffect, useCallback } from 'react' // useRef is to store a value that persists after each render
 import UseIntersect from './useIntersect2'
 
+const apiURL = 'nyc_ttp_pins.json'
+const resultsPerPage = 10
 function App() {
-  // const [query, setQuery] = useState('')
-  // const [startSliceNumber, setStartSliceNumber] = useState(0)
-  // const [endSliceNumber, setEndSliceNumber] = useState(10)
-
-  // const {error, pins, hasMore } = useIntersect(query, startSliceNumber, endSliceNumber)
-
-  // const loader = useRef(null) // null initially - then set a reference to the last book element
-
-  // useEffect(() => {
-  //   const options = {
-  //     root: null,
-  //     // rootMargin: "20px",
-  //     threshold: 0
-  //   }
-
-  //   // if (loader.current) loader.current.disconnect()
-
-  //    const observer = new IntersectionObserver(entries => {
-  //     // console.log(33, hasMore)
-  //     // console.log(34, entries[0].isIntersecting)
-
-  //     // Only observing one target element 
-  //     if (entries[0].isIntersecting && hasMore) {
-  //       console.log('Visible')
-  //       setStartSliceNumber(prevStartSliceNumber => prevStartSliceNumber + 10)
-  //       console.log(38, startSliceNumber)
-  //       setEndSliceNumber(prevEndSliceNumber => prevEndSliceNumber + 10)
-  //       console.log(40, endSliceNumber)
-  //     }
-  //   }, options)
-
-  //   observer.observe(loader.current)
-  // }, [hasMore])
-  // const lastPinElementRef = useCallback(node => {
-  //   const options = {
-  //     root: null,
-  //     // rootMargin: "20px",
-  //     threshold: 0
-  //   }
-
-  //   // Disconnect the reference from the previous last pin 
-  //   if (observer.current) observer.current.disconnect()
-
-  //   observer.current = new IntersectionObserver(entries => {
-  //     // Only observing one target element 
-  //     console.log(33, hasMore)
-  //     console.log(34, entries[0].isIntersecting)
-  //     if (entries[0].isIntersecting && hasMore) {
-  //       console.log('Visible')
-  //       setStartSliceNumber(prevStartSliceNumber => prevStartSliceNumber + 10)
-  //       console.log(38, startSliceNumber)
-  //       setEndSliceNumber(prevEndSliceNumber => prevEndSliceNumber + 10)
-  //       console.log(40, endSliceNumber)
-  //     }
-  //   }, options)
-
-  //   // If we have a last element, we want to reference it and observe that element 
-  //   if (node) observer.current.observe(node)
-
-  // }, [hasMore])
-
-
-
-  // const handleQuery = (event) => {
-  //   setQuery(event.target.value) // Each letter a user types in the input box will update the query, which will call useIntersect 
-
-  //   // Reset the page back to the beginning
-  //   setStartSliceNumber(0) 
-  //   setEndSliceNumber(10)
-  // }
-// 19 false, 64 true, 33 true, 34 false  
-// 33 true 34 true Visible 102 undefined undefined 112 true 19 false 19 false 33 true 34 false
-// 33 true 34 true Visible 102 undefined undefined 112 true
-
-  // return (
-  //   <div>
-  //     <h1>Pinterest</h1>
-  //     <input type="text" value={query} onChange={handleQuery}></input>
-  //     {/* <PinterestPins/> */}
-      
-  //     <div>{
-  //         pins.map((pin, index) => {
-  //           if (pins.length === index + 1) { // This means we are at our last pin
-  //             return (
-  //               <>
-  //               <div style={{width: '200px', height: '100px', backgroundColor:'yellow'}} key={pin.id}>{pin.title}</div>
-  //               <p>**************************</p>
-  //               </>
-  //             )
-  //           } else {
-  //             return (
-  //               <>
-  //               <div style={{width: '200px', height: '100px', backgroundColor:'yellow'}} key={pin.id}>{pin.title}</div>
-  //               <p>--------------------------</p>
-  //               </>
-  //             )
-  //           }
-  //         })
-  //     }</div>
-
-  //     <div ref={loader} >Loading...</div>
-
-  //   </div>
-  // );
+  
   return (
-    <UseIntersect />
+    <UseIntersect url={apiURL} resultsPerPage={resultsPerPage} />
   )
 }
 
--- src/useIntersect2.jsx
+++ src/useIntersect2.jsx
@@ -1,20 +1,19 @@
 import { useEffect, useState, useRef } from 'react'
 
-export default function UseIntersect() { // When your scrolling hits the bottom, fetch the next page of results
+export default function UseIntersect({url, resultsPerPage}) { // When your scrolling hits the bottom, fetch the next page of results
     const [startSliceNumber, setStartSliceNumber] = useState(0)
-    const [prevY, setPrevY] = useState(0)
 
     const [loading, setLoading] = useState(true)
     const [error, setError] = useState(false)
     const [pins, setPins] = useState([])
-    const [allPins, setAllPins] = useState([])
+
     const [hasMore, setHasMore] = useState(false)
 
 
     useEffect(() => {
-        // Every time we are making a brand new request we do not have an error so set error to false
-        setLoading(true)
-        setError(false)
+
+        setLoading(true) // We are making a request, so loading is true
+        setError(false) // Every time we are making a brand new request we do not have an error so set error to false
 
         const abortController = new AbortController()
         const signal = abortController.signal
@@ -22,7 +21,7 @@ export default function UseIntersect() { // When your scrolling hits the bottom,
         const getPins = async () => {
 
             try {
-                const pinsResponse = await fetch('nyc_ttp_pins.json', {
+                const pinsResponse = await fetch(`${url}`, {
                     method: 'GET',
                     headers: {
                         'Content-Type': 'application/json',
@@ -35,14 +34,7 @@ export default function UseIntersect() { // When your scrolling hits the bottom,
                 const pinsJSON = await pinsResponse.json()
                 console.log(pinsJSON)
 
-                
-                // setAllPins(pinsJSON)
-                // Get only 100 or less results every time we scroll to the bottom
-                // setPins(pinsJSON.slice(startSliceNumber, startSliceNumber+10))
-                // console.log(40, !(startSliceNumber >= pinsResponse.length))
-                // setHasMore(!(startSliceNumber >= pinsResponse.length))
-
-                const nextPins = pinsJSON.slice(startSliceNumber, startSliceNumber+10)
+                const nextPins = pinsJSON.slice(startSliceNumber, startSliceNumber+resultsPerPage)
 
                 setPins(prevPins => {
                     return [...new Set([...prevPins, ...nextPins])]
@@ -66,48 +58,27 @@ export default function UseIntersect() { // When your scrolling hits the bottom,
         return () => abortController.abort()
 
     }, [startSliceNumber])
-    
-    // useEffect(() => {
-
-    //     const getNextPins =  () => {
-
-    //         console.log(64, startSliceNumber)
-    //         // Get only 10 or less results every time we scroll to the bottom
-    //         const nextPins = allPins.slice(startSliceNumber, startSliceNumber+10)
-
-    //         setPins(prevPins => {
-    //             return [...new Set([...prevPins, ...nextPins.map(pin => pin)])]
-    //         })
-
-    //         // console.log(72, !(startSliceNumber >= allPins.length))
-            
-    //         setHasMore(!(startSliceNumber >= allPins.length))
-            
-    //     }
-        
-    //     getNextPins()
-
-    // }, [startSliceNumber]) // Want to fetch results everytime we do a search or scrolling
 
 
-    const loader = useRef(null) // null initially - then set a reference to the last book element
+    const loader = useRef(null) // null initially - then a reference to loading element
 
-    const handleObserver = entries => {
-        // Only observing one target element 
-        if (entries[0].isIntersecting) {
-            console.log('Visible')
-            setStartSliceNumber(prevStartSliceNumber => prevStartSliceNumber + 10)
 
-        }
-    }
     const options = {
         root: null,
         rootMargin: "0px",
         threshold: 0
     }
+    const handleObserver = entries => {
+        // Only observing one target element, the loading element
+        if (entries[0].isIntersecting) {
+            console.log('Visible')
+            setStartSliceNumber(prevStartSliceNumber => prevStartSliceNumber + resultsPerPage)
+        }
+    }
+
     useEffect(() => {
         console.log(109, loading, hasMore)
-        if (!hasMore) return 
+        if (!hasMore) return // If we have reached 
 
         const observer = new IntersectionObserver(handleObserver, options)
 
@@ -129,20 +100,20 @@ export default function UseIntersect() { // When your scrolling hits the bottom,
             <h1>Pinterest</h1>
             
             <div>
-            {
-                pins.map((pin, index) => {
-                    
-                    return (
-                        <>
-                        <div style={{width: '200px', height: '100px', backgroundColor:'yellow'}} key={pin.id}>{pin.title}</div>
-                        <p>--------------------------</p>
-                        </>
-                    )
-                    
-                })
-            }
-            {/* <div ref={loader}> <h2 style={{color: 'red'}}>{hasMore && 'Loading...'}</h2> </div> */}
-            <div ref={loader} style={{display: hasMore? 'block': 'none'}}>Loading...</div>
+                {
+                    pins.map((pin, index) => {
+                        
+                        return (
+                            <>
+                            <div style={{width: '200px', height: '100px', backgroundColor:'yellow'}} key={pin.id}>{pin.title}</div>
+                            <p>--------------------------</p>
+                            </>
+                        )
+                        
+                    })
+                }
+
+                <div ref={loader} style={{display: hasMore? 'block': 'none'}}>Loading...</div>
             </div>
         
             
